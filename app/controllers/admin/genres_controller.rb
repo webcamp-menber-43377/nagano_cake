63d9f3d85b8504cc96e1411c37937f5362e45efe
Shiori-S-92 <106877474+Shiori-S-92@users.noreply.github.com>
2022-08-28 10:15:58 +0000

    [Add] admin｜ジャンル一覧・追加画面

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,13 +1,29 @@
 class Admin::GenresController < ApplicationController
   def index
+    @genre = Genre.new
+    @genres = Genre.all
   end
 
   def create
+    @genre = Genre.new(genre_params)
+    if @genre.save
+      redirect_to admin_genres_path(@genre.id)
+    else
+      @genres = Genre.all
+      render :index
+    end
   end
 
   def edit
+    @genre = Genre.find(params[:id])
   end
 
   def update
+  end
+
+  private
+  # ストロングパラメータ
+  def genre_params
+    params.require(:genre).permit(:name)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,63 +1,4 @@
 Rails.application.routes.draw do
-  namespace :admin do
-    get 'order_details/update'
-  end
-  namespace :admin do
-    get 'orders/show'
-    get 'orders/update'
-  end
-  namespace :admin do
-    get 'customers/index'
-    get 'customers/show'
-    get 'customers/edit'
-    get 'customers/update'
-  end
-  namespace :admin do
-    get 'genres/index'
-    get 'genres/create'
-    get 'genres/edit'
-    get 'genres/update'
-  end
-  namespace :admin do
-    get 'items/index'
-    get 'items/new'
-    get 'items/create'
-    get 'items/show'
-    get 'items/edit'
-    get 'items/update'
-  end
-  namespace :admin do
-    get 'homes/top'
-  end
-  namespace :public do
-    get 'shipping_addresses/index'
-    get 'shipping_addresses/edit'
-    get 'shipping_addresses/create'
-    get 'shipping_addresses/update'
-    get 'shipping_addresses/destroy'
-  end
-  namespace :public do
-    get 'orders/new'
-    get 'orders/confirm'
-    get 'orders/complete'
-    get 'orders/create'
-    get 'orders/index'
-    get 'orders/show'
-  end
-  namespace :public do
-    get 'cart_items/index'
-    get 'cart_items/update'
-    get 'cart_items/destroy'
-    get 'cart_items/destroy_all'
-    get 'cart_items/create'
-  end
-  namespace :public do
-    get 'customers/show'
-    get 'customers/edit'
-    get 'customers/update'
-    get 'customers/unsubscribe'
-    get 'customers/withdraw'
-  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   # 顧客用
@@ -78,6 +19,11 @@ Rails.application.routes.draw do
     root to: "homes#top"
     # get 'homes/top'
     get "/about" => "homes#about", as: "about"
+  end
+
+  namespace :admin do
+    # get 'homes/top'
+    resources :genres, only: [:index, :edit, :create]
   end
 
 end
