760e07e3c8a594fe0a4479d2184d56f20240b92a
Shiori-S-92 <106877474+Shiori-S-92@users.noreply.github.com>
2022-09-01 15:25:58 +0000

    [Add] 管理者_3.1.6_商品詳細画面

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,22 +1,28 @@
 class Admin::ItemsController < ApplicationController
   def index
+    @items = Item.all
   end
 
   def new
-    @item = Item.new
+    @item = Item.new(item_params)
   end
 
   def create
     @item = Item.new(item_params)
     if @item.save
-
+      redirect_to admin_item_path(@item.id)
     else
-
+      render :new
     end
   end
 
   def show
+    @item = Item.find(params[:id])
   end
+
+  # def add_tax_price
+  #   (self.price * 1.10).round
+  # end
 
   def edit
   end
--- config/routes.rb
+++ config/routes.rb
@@ -24,7 +24,7 @@ Rails.application.routes.draw do
   namespace :admin do
     # get 'homes/top'
     resources :genres, only: [:index, :create, :edit, :update]
-    resources :items, only: [:new, :index, :create]
+    resources :items, only: [:new, :index, :create, :show, :edit]
   end
 
 end
