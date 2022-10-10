2daf7b7bf960214ed54663e2474d53f70263eafe
Shiori-S-92 <106877474+Shiori-S-92@users.noreply.github.com>
2022-10-10 13:39:04 +0000

    [Update] 3.2.12_カート画面, 個数追加

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -9,22 +9,32 @@ class Public::CartItemsController < ApplicationController
   def create
     # binding.pry
     @cart_item = current_customer.cart_items.new(cart_item_params)
-    ## もし元々カート内に「同じ商品」がある場合、「数量を追加」更新・保存する
-      ##元々カート内にあるもの「item_id」
-      ##今追加した  params[:cart_item][:item_id]
-    # if current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id]).present?
-    #   ##cart_item.quantityに今追加したparams[:cart_item][:quantity]を加える
-    #   ##.to_iとして数字として扱う
-    #   cart_item = current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
-    #   cart_item.quantity += params[:cart_item][:quantity].to_i
 
-    #   cart_item.save
-    #   redirect_to cart_items_path
+    ## 1. 追加した商品がカート内に存在するかの判別
+          ##元々カート内にあるもの「item_id」
+          ##今追加した  params[:cart_item][:item_id]
+    if current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id]).present?
+      ## 存在した場合
+        ## 2. カート内の個数をフォームから送られた個数分追加する
+        ## もし元々カート内に「同じ商品」がある場合、「数量を追加」更新・保存する
+      ##cart_item.quantityに今追加したparams[:cart_item][:quantity]を加える
+      cart_item = current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
+      ##.to_iとして数字として扱う
+      cart_item.amount += params[:cart_item][:amount].to_i
 
-    ## もしカート内に「同じ」商品がない場合は通常の保存処理
-    @cart_item.save
-    @cart_items = current_customer.cart_items.all
-    redirect_to cart_items_path
+      cart_item.save
+      redirect_to cart_items_path
+    elsif
+      ## 存在しなかった場合
+        ## カートモデルにレコードを新規作成する
+        ## もしカート内に「同じ」商品がない場合は通常の保存処理
+      @cart_item.save
+      @cart_items = current_customer.cart_items.all
+      redirect_to cart_items_path
+    else
+      # 保存できなかった場合
+      redirect_to cart_items_path
+    end
   end
 
   def update
