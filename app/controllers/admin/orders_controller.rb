d3a86c9f361d01d503a8b1be8ed5d8129b5933f3
Shiori-S-92 <106877474+Shiori-S-92@users.noreply.github.com>
2022-10-20 09:18:33 +0000

    [Update] 商品一覧

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,9 +1,19 @@
 class Admin::OrdersController < ApplicationController
   def show
-    @customer = Customer.find(params[:id])
-    @order_detail = OrderDetail.find(params[:id])
+    # @customer = Customer.find(params[:id])
+    @order = Order.find(params[:id])
+    # @order_detail = OrderDetail.find(params[:id])
   end
 
   def update
+  end
+
+  private
+  # ストロングパラメータ
+  def order_params
+    params.require(:order).permit(:customer_id, :postal_code, :address, :name, :shipping_cost, :total_payment, :payment_method)
+  end
+  def order_detail_params
+    params.require(:order_detail).permit(:order_id, :item_id, :price, :amount, :making_status)
   end
 end
